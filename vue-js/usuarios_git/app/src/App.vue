db91f5edb20eb1ed322095d7d8e1aa8ea42ac142
Andre de Faria <adfariacarvalho@gmail.com>
2020-12-24 10:54:00 -0400

    brincando com o git

---
+++ vue-js/usuarios_git/app/src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="app">
+    <div class="container">
+      <h1>Usuarios do Git</h1>
+      <div class="row">
+        <div v-for="user of users" :key="user.id" class="card col-6 m-2" style="width: 15rem;">
+          <img class="card-img-top" :src="user.avatar_url" alt="Card image cap">
+          <div class="card-body">
+            <h5 class="card-title">#{{user.id}} - {{user.login}}</h5>
+            <a :href="user.html_url" class="btn btn-primary">Link do GitHub</a>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+  </div>
+</template>
+
+<script>
+import User from './services/users'
+
+export default {
+
+  data(){
+    return{
+      users: this.users
+    }
+  },
+
+  mounted(){
+    User.listar().then(resposta => {
+      console.log(resposta.data);
+      this.users = resposta.data;
+    })
+  }
+
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
