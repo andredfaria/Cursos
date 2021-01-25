8912bdee3885dab4c3a7b7f7c9851a749d458d65
André <adfariacarvalho@gmail.com>
2021-01-24 23:51:48 -0300

    finalização da aplicação, salvando em localstorege

--- vue-js/vue_app/js/app.js
+++ vue-js/vue_app/js/app.js
@@ -1,20 +1,81 @@
 const app = new Vue({
     el: '#app',
     data: {
+        contacts: [],
         contact: {
-            name:'eu mesmo',
+            id: '',
+            name:'',
             email: '',
             phone:''
         },
+        edit: false,
         msg: 'Hello World!',
-        image: '/icon.png',
-        visible: true,
         name: 'Contatos App'
 
     },
     methods: {
-        saveContact(contact) {
-            console.log(this.contact)
+        saveContact(c) {
+            let contacts = localStorage.getItem('contactsApp');
+
+            c.id = new Date().getTime();
+
+            if (contacts) {
+                contacts = JSON.parse(contacts);
+                contacts.push(c);
+            } else {
+                contacts = [c];
+            }
+            
+            this.contacts = contacts;
+
+            localStorage.setItem('contactsApp', JSON.stringify(contacts));
+
+            location.reload();  
+            
+            this.contacts = [];
+        },
+
+        deleteContact(id) {
+            let contacts = localStorage.getItem('contactsApp')
+            
+            if (!contacts)
+                return;
+            
+            contacts = JSON.parse(contacts);
+
+            contacts = contacts.filter((c) => {
+                return c.id != id;
+            });
+            
+            this.contacts = contacts;
+
+            localStorage.setItem('contactsApp', JSON.stringify(contacts))
+        },
+
+        editContact(contact) {
+            this.contact = contact;     
+            this.edit = true;
+        },
+
+        updateContact(contact) {
+            let contacts = this.contacts.map(c => {
+                if (c.id == contact.id)
+                    return contact;
+                
+                return c;
+            });
+
+            this.edit = false;
+
+            this.contacts = contacts;
+
+            localStorage.setItem('contactsApp').JSON.stringify(contacts);
+
+            location.reload();
+            
         }
+    },
+    created() {
+        this.contacts = JSON.parse(localStorage.getItem('contactsApp'));
     }
 })
