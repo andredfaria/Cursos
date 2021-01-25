8dde4191916664340f2bd466104fe2649af5ddce
André <adfariacarvalho@gmail.com>
2021-01-24 22:57:25 -0300

    começando a aplicação

--- vue-js/vue_app/js/app.js
+++ vue-js/vue_app/js/app.js
@@ -1,9 +1,20 @@
 const app = new Vue({
     el: '#app',
     data: {
+        contact: {
+            name:'eu mesmo',
+            email: '',
+            phone:''
+        },
         msg: 'Hello World!',
         image: '/icon.png',
-        visible: true
+        visible: true,
+        name: 'Contatos App'
 
+    },
+    methods: {
+        saveContact(contact) {
+            console.log(this.contact)
+        }
     }
 })
