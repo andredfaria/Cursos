662f82280123a89263162822dc1a1fcd723a37ca
André de Faria <andre.faria@weclever.co>
2021-02-23 23:00:40 -0300

    finalização com a trava de usuario

--- node/nlw-4-node/api/src/controller/UserController.ts
+++ node/nlw-4-node/api/src/controller/UserController.ts
@@ -1,11 +1,30 @@
 import { Request, Response } from 'express';
+import { getRepository } from 'typeorm';
+import { User } from '../models/User';
 
 class UserController {
     async create (req: Request, res: Response) {
-        const body = req.body;
-        console.log(body);
-        res.send(body)
-        
+        const {name, email} = req.body;
+
+        const usersRepository = getRepository(User);
+
+        const userAlreadyExists = await usersRepository.findOne({
+            email
+        })
+
+        const user = usersRepository.create({
+            name, email
+        });
+
+        if(userAlreadyExists){
+            return res.status(400).json({
+                mesage: 'User already exists !'
+            })
+        }
+
+        await usersRepository.save(user);
+
+        return res.json(user);
     }
 }
 
