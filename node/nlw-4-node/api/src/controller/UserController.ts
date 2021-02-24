a3e5c82e8d680c34e26f252201884824272a52f0
André de Faria <andre.faria@weclever.co>
2021-02-23 22:43:16 -0300

    inicializando a aplicação  e conectando com o banco sqlite

---
+++ node/nlw-4-node/api/src/controller/UserController.ts
@@ -0,0 +1,12 @@
+import { Request, Response } from 'express';
+
+class UserController {
+    async create (req: Request, res: Response) {
+        const body = req.body;
+        console.log(body);
+        res.send(body)
+        
+    }
+}
+
+export { UserController };
