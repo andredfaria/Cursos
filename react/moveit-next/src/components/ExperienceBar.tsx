1329cc5cbd5845548e30d036a615b28fabd097f4
André de Faria <adfariacarvalho@gmail.com>
2021-02-27 02:33:38 -0300

    merge with application in react

---
+++ react/moveit-next/src/components/ExperienceBar.tsx
@@ -0,0 +1,17 @@
+import styles from '../styles/components/ExperienceBar.module.css'
+  
+export function ExperienceBar() {
+  return (
+    <div className={styles.container}>
+      <header className={styles.experienceBar}>
+        <span>0 xp</span>
+        <div>
+          <div style={{width: '68%'}}></div>  
+
+          <span className={styles.currentExperience} style={{left: '50%'}}>300px</span>
+        </div>
+        <span>600 xp</span>
+      </header>
+    </div>
+  );
+}
