cb6a0963481cc7ad19fb9e53a2c133886855cf25
Andre de Faria <adfariacarvalho@gmail.com>
2021-04-22 00:47:34 -0300

    third day nlw

--- react/podcaster/.next/static/chunks/webpack.js
+++ react/podcaster/.next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "d9c28a2d8aa067539c24";
+/******/ 	var hotCurrentHash = "288ad7c91cc957ef3c1d";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- react/podcaster/src/components/Header/index.tsx
+++ react/podcaster/src/components/Header/index.tsx
@@ -1,13 +1,16 @@
 import styles from "./styles.module.scss";
 import format  from "date-fns/format";
 import ptBR from "date-fns/locale/pt-BR";
+import Link from 'next/link';
 
 export function Header() {
   let data = format(new Date(), "EEEEEE, d MMMM", { locale: ptBR} )
 
   return (
     <header className={styles.headerContainer}>
-      <img src="/logo.svg" alt="Podcaster" />
+      <Link href='/'>
+        <img src="/logo.svg" alt="Podcaster" />
+      </Link>
       
       <p>O melhor para você ouvir, sempre</p>
 
