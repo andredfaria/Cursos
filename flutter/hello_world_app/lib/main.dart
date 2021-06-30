a606c7ba7f136a143afae62dafd5a9113aa1e02b
Andre de Faria <adfariacarvalho@gmail.com>
2021-06-30 00:03:18 -0300

    gitflow-feature-stash: flutter

---
+++ flutter/hello_world_app/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'screens/login/login_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Animations',
+      debugShowCheckedModeBanner: false,
+      home: LoginScreen(),
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        primaryColor: const Color(0xFFFF4D00),
+        accentColor: const Color(0xFFFF4D00),
+        fontFamily: 'OpenSans',
+      )
+    );
+  }
+}
